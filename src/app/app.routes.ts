276cc69e4ea484df0449ffb10d467aeffc493230
Laura Moreno <chipifsdev@gmail.com>
2025-05-08 20:56:15 +0200

    Añadido router-outlet> al app.component // añadido provideHttpClient y WithComponentInputBinding al app.config // puesta primera ruta hacia proyectos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ProyectosComponent } from './pages/proyectos/proyectos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'proyectos', component: ProyectosComponent}
+];
